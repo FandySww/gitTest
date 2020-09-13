7b17c722005e91b97a45cac64c78ee45548a84af
FandySww <fa370@sina.com>
2020-09-13 19:05:17 +0800

    添加

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,6 +1,6 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("123,123");
-        System.out.println("dev");
+        System.out.println("dev-¹¤×÷Çø");
     }
 }
